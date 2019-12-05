ef689bdd00538d00905a3d0293cced0257a5e0bc
Winston Pemberton <winstonpemberton@gmail.com>
2019-12-05 23:43:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -17,7 +18,9 @@ class Scraper
   end
 
   def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
+    binding.pry
   end
 
 end
